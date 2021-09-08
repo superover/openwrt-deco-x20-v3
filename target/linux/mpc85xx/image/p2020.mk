8cd2ad62ab55c1d77a9b6b66fe44593e1c124626
Damien Mascord <tusker@tusker.org>
2021-09-08 15:13:03 +1000

    mpc85xx: add support for WatchGuard XTM330
    This patch adds support for the WatchGuard XTM330.
    
    Specifications:
     * SoC: 1Ghz Freescale P2020 e500v5
     * RAM: 1 GiB DDR3, 32-bit, ECC off
     * Flash: 2MB MB SPI NOR + 512MB NAND
     * WiFi: None (Mini PCIe on board)
     * Ethernet: 1 x 1GbE-WAN + 6 x 1GbE-LAN on Marvell 88E6171
     * Display LCD
     * UART: 115200,8,N,1 RJA45 console
    
    Installation procedure:
     * Switch jumper 1 to position 1-2
     * setenv ipaddr 192.168.1.1
     * setenv serverip 192.168.1.2
     * tftpboot openwrt-mpc85xx-p2020-watchguard_xtm-330-initramfs-kernel.bin
     * bootm
     * sysupgrade openwrt-mpc85xx-p2020-watchguard_xtm-330-squashfs-sysupgrade.bin
    
    Signed-off-by: Damien Mascord <tusker@tusker.org>

--- target/linux/mpc85xx/image/p2020.mk
+++ target/linux/mpc85xx/image/p2020.mk
@@ -13,3 +13,18 @@ define Device/freescale_p2020rdb
 	pad-rootfs $$(BLOCKSIZE) | append-metadata
 endef
 TARGET_DEVICES += freescale_p2020rdb
+
+define Device/watchguard_xtm-330
+  DEVICE_VENDOR := WatchGuard
+  DEVICE_MODEL := XTM 330
+  DEVICE_PACKAGES := kmod-hwmon-lm90 kmod-rtc-ds1307 \
+	kmod-gpio-pca953x kmod-eeprom-at24
+  BLOCKSIZE := 128k
+  KERNEL := kernel-bin | gzip | \
+	fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb
+  SUPPORTED_DEVICES := fsl,P2020RDB
+  IMAGES := sysupgrade.bin
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
+	pad-rootfs $$(BLOCKSIZE) | append-metadata
+endef
+TARGET_DEVICES += watchguard_xtm-330
